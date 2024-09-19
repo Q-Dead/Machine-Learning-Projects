5b09577be730441bec36e93dd22a65f0cc9f420d
Q-Dead <jasperjohnjaso6969@gmail.com>
2024-09-19 14:56:05 +0800

    Modified Softmax Function and add intercept (bias)

--- Multi-Class Classification/MultiClassLogisticRegressionAlgo.py
+++ Multi-Class Classification/MultiClassLogisticRegressionAlgo.py
@@ -12,24 +12,34 @@ class MultiClassLogisticRegression:
         i, n = X.shape
         j, k = y.shape
         self.weight = np.zeros((n, k))
-        self.intercept = 0
+        self.intercept = np.zeros(k)
 
         for _ in range(self.n_iter):
 
-            Z = X @ self.weight
+            Z = np.dot(X, self.weight) + self.intercept
             y_hat = self.softmax(Z)
 
-            dbdw = (1/i) * np.dot(X.T, ( y_hat - y))
+            dbda = (1/i) * np.dot(X.T, ( y_hat - y))
+            dbdb = (1/i) * sum(y_hat - y)
 
-            self.weight = self.weight - self.learning_rate * dbdw
+            self.weight = self.weight - self.learning_rate * dbda
+            self.intercept = self.intercept - self.learning_rate * dbdb
 
     def softmax(self, Z):
+        i, n = Z.shape
+        Z_out_container = np.empty((i, n))
+        for i in range(Z.shape[0]):
 
-        max_value = np.amax(Z)
-        exp_ti = np.exp(Z - max_value)
-        return exp_ti / np.sum(exp_ti)
+            max_value = np.amax(Z[i])
+            exp_ti = np.exp(Z[i] - max_value)
+            Z_out_container[i] = exp_ti / np.sum(exp_ti)
+
+        return Z_out_container
+        
 
     def predict(self, X):
-        Z = X @ self.weight
-        return self.softmax(Z)
+        Z = np.dot(X, self.weight) + self.intercept
+        probability_pred = self.softmax(Z)
+        return (probability_pred == probability_pred.max(axis=1, keepdims=True)).astype(float)
+            
 
